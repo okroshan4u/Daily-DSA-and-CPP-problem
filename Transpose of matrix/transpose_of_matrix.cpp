3c3a4ee2f552f1b788bd6dcfacbf9ca84ee4517a
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-09-27 19:41:46 +0530

    Uploaded the transpose of matrix

---
+++ Transpose of matrix/transpose_of_matrix.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size(); // number of rows
+        int n = matrix[0].size(); // number of columns
+
+        vector<vector<int>> result(n, vector<int>(m)); // n x m
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    // Example input
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    vector<vector<int>> result = sol.transpose(matrix);
+
+    // Print result
+    cout << "Transposed matrix:" << endl;
+    for (const auto& row : result) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
