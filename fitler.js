ae53093805ff52f34ca67142bbf8e641dd2aa015
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-12-04 19:07:08 +0530

    used fiter method

---
+++ fitler.js
@@ -0,0 +1,15 @@
+const users = [
+  { name: "Roshan", role: "Engineer", active: true },
+  { name: "Dinesh", role: "Designer", active: false },
+  { name: "Mahesh", role: "Engineer", active: false },
+  { name: "Priya", role: "Engineer", active: true }
+];
+// const activeUsers = users.filter((user)=> user.role === "Engineer" && user.active ===  true).sort((a, b)=> a.name.localeCompare(b.name));
+// console.log(activeUsers);
+
+// const ordereduser = users.sort((a,b)=> a.name.localeCompare(b.name))
+// console.log(ordereduser)
+
+const notactiveusers= users.filter((user)=> user.role === "Engineer" && user.active === true).sort((a,b)=> b.name.localeCompare(a.name))
+
+console.log(notactiveusers);
