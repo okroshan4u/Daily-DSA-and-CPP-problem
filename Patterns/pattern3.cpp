078df1ccb9b3b457622fac24559addcd7df7ea89
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-06-29 21:28:10 +0530

    Add files via upload

---
+++ Patterns/pattern3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    int i = 1;
+
+    cout<<"Enter the number n: "<<endl;
+    cin>>n;
+
+    while(i<=n){
+        int j = 1;
+        while(j<=n){
+            cout<<j;
+            j++;
+        }
+        cout<<"\n";
+        i++;
+    }
+
+    return 0;
+}
