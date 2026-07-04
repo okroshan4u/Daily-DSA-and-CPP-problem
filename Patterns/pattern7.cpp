0f61d31d8118264d7e70bb660721f91317a29560
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-07-04 21:53:57 +0530

    Add files via upload

---
+++ Patterns/pattern7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    int i = 1;
+    int count = 1;
+
+    cout<<"Enter the number n: "<<endl;
+    cin>>n;
+
+    while(i<=n){
+        int j =  1;
+        while(j<=i){
+            cout<<i;
+            j++;
+        }
+        cout<<"\n";
+        i++;
+    }
+
+    return 0;
+}
