93fc8b0f0e8555821bda5a8fe102f44389e35a44
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-07-06 21:45:42 +0530

    Add files via upload

---
+++ Patterns/pattern10.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    int i = 1;
+
+    cout<<"Enter the number n: "<<endl;
+    cin>>n;
+
+    // while(i<=n){
+    //     int j =  0;
+    //     while(j<i){
+    //         cout<< i - j<<" ";
+    //         j++;
+    //     }
+    //     cout<<"\n";
+    //     i++;
+    // }
+
+    // or
+
+    while(i<=n){
+        int j =  1;
+        while(j<=i){
+            cout<< i - j + 1<<" ";
+            j++;
+        }
+        cout<<"\n";
+        i++;
+    }
+
+
+    return 0;
+}
