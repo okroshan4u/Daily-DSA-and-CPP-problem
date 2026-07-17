c871ba6ce92358e16aa5238bdb753051dd26a1e9
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-07-17 23:34:42 +0530

    Add files via upload

---
+++ Patterns/loops.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+// sum to first n numbers 
+    // int n ;
+    // cin>>n;
+    // int sum = 0;
+
+    // while(n>0){
+    //     sum = sum + n;
+    //     n = n - 1;
+    // }
+    // cout<<sum<<endl;
+
+// sum of first even numbers
+
+    // int n;
+    // cin>>n;
+    // int sum  = 0;
+    // while(n>0){
+    //     if(n%2==0){
+    //         sum = sum + n;
+    //     }
+    //     n--;
+    // }
+    // cout<<sum<<endl;
+
+
+// Farenheit to celsius temp 
+// (c/5) = (f-32)/9
+float c ;
+float f ;
+
+cout<<"Enter the Farenheit temp: "<<endl;
+cin>>f;
+
+// cout<<"Enter the ce temp: "<<endl;
+// cin>>c;
+
+ c = (5*(f-32))/9;
+
+ cout<<"The temp in celsius is : "<<c<<endl;
+     
+
+
+
+
+    return 0;
+}
