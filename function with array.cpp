a49abccf3c0b950167c58fd3543cd4f3809238e5
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-07-24 21:51:31 +0530

    Add files via upload

---
+++ function with array.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n){
+    for(int i = 0 ; i< n ; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main() {
+    int n ;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n;
+    int arr[n] = {1};
+    
+    printArray(arr, n);
+    cout<<"\n";
+    int arrSize = sizeof(arr)/sizeof(int);
+    cout<<"Size of arr is : "<<arrSize<<endl;
+
+    return 0;
+}
