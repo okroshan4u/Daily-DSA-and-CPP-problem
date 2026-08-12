dec93108afa2a90aaf3b260baa824eb7c8fef292
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-08-12 21:43:24 +0530

    Fix formatting issues in pairSum.cpp

--- pairSum.cpp
+++ pairSum.cpp
@@ -2,7 +2,7 @@
 
 vector<vector<int>> pairSum(vector<int> &arr, int s){
 
-   vector<vector<int>> ans
+   vector<vector<int>> ans;
 
    for(int i = 0 ; i < arr.size() ; i++){
       for(int j = i + 1 ; j <= arr.size() - 1; j++){
