c7c85854e7e0067084205d8803467513312cda37
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-08-20 21:30:53 +0530

    Add files via upload

---
+++ Binary search.cpp
@@ -0,0 +1,31 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace  std;
+
+int binarySearch(int arr[],int size ,int key){
+    int start=  0;
+    int end = size - 1;
+
+    int mid = (start + end)/2;
+    while(start <= end){
+        if(arr[mid]>key){
+            end = mid -1 ;
+        }
+        if(arr[mid]<key){
+            start = mid + 1;
+        }
+        if(arr[mid]==key){
+            return mid;
+        }
+        mid = (start + end )/2;
+        
+    }
+    return -1;
+}
+int main() {
+    int arr[]= {1,2,3,4,5,6,7,8};
+
+    cout<<binarySearch(arr,8,6);
+
+    return 0;
+}
