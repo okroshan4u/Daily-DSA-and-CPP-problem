3661918b82200d456492ebc72d2a935cc10c409d
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-08-22 20:54:27 +0530

    Add files via upload

---
+++ lru.cpp
@@ -0,0 +1,45 @@
+class LRUcache{
+    constructor (capacity){
+        this.capacity = capacity;
+        this.cache = new Map();
+    }
+   
+    get(key){
+        if(!this.cache.has(key)){
+            return -1;
+        }
+       
+        //get the value
+        const value = this.cache.get(key);
+       
+        //move key at the end = most recently used
+        this.cache.delete(key);
+        this.cache.set(key,value);
+       
+        return value;
+    }
+   
+    put(key, value){
+        // if key already exist then remove it first
+        if(this.cache.has(key)){
+            this.cache.delete(key);
+        }
+        //add it to the end = most recently used
+        this.cache.set(key, value);
+       
+        // if capacity exceeded , remove first item
+       
+        if(this.cache.size > this.capacity){
+            const firstKey= this.cache.keys().next().value;
+            this.cache.delete(firstKey);
+        }
+    }
+   
+}
+
+
+const lru = new LRUcache(3);
+
+lru.put("A",1);
+lru.put("B",2);
+lru.put("C",3);
