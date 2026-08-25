b044121bcdf87acc2f98948c5eb0b7be713dc19c
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-08-25 22:27:11 +0530

    Add files via upload

---
+++ bounds.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstOccurance(int arr[], int size , int key){
+    int s = 0;
+    int e = size - 1;
+    int ans = -1;
+    int mid = s + (e - s) / 2;
+    while(s<=e){
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
+        }
+        if(key > arr[mid]){
+            s = mid + 1;
+        }
+        else if(key < arr[mid]){
+            e = mid - 1;
+        }
+        mid = s + (e - s)/2;
+    }
+    return ans;
+}
+
+int lastOccurance(int arr[], int size , int key){
+    int s = 0;
+    int e = size - 1;
+    int ans = -1;
+    int mid = s + (e - s) / 2;
+    while(s<=e){
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }
+        if(key > arr[mid]){
+            s = mid + 1;
+        }
+        else if(key < arr[mid]){
+            e = mid - 1;
+        }
+        mid = s + (e - s)/2;
+    }
+    return ans;
+}
+
+
+int main() {
+// your code goes here
+int arr[] = {1,2,3,4,4,5,6};
+
+cout<<firstOccurance(arr, 8,4)<<" ";
+cout<<lastOccurance(arr, 8,4);
+
+cout<<"Total number of occurances in the array is : "<<lastOccurance(arr, 8,4) - firstOccurance(arr, 8,4) + 1;
+
+}
