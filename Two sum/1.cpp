576bd58b3d20fd8978f17e94d9c44b47bd4d3cb7
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-08-27 21:14:09 +0530

    Update 1.cpp

--- Two sum/1.cpp
+++ Two sum/1.cpp
@@ -4,7 +4,7 @@
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        // Map to store: Key = number, Value = its ind
+        // Map to store: Key = numb
         std::unordered_map<int, int> num_map;
         
         for (int i = 0; i < nums.size(); ++i) {
