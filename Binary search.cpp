9a81e9aafc55109077cbad51e45e9ae945e46c73
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-08-29 19:25:22 +0530

    Fix formatting issues in binary search implementation

--- Binary search.cpp
+++ Binary search.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace  std;
-// implementation of 
+// implementation of binary search
 int binarySearch(int arr[],int size ,int key){
     int start=  0;
     int end = size - 1;
